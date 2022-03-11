0b3fe4108719c680a9def7fdf9e29a08e9eb542a
iv4pa4 <ivayla.panayotova@gmail.com>
2022-03-11 14:58:42 +0200

    errors fix

--- infopage.php
+++ infopage.php
@@ -7,8 +7,12 @@ include("base_connection.php");
 </head>
 <body>
 <h1>Какво си търсиш?</h1>
+<a href="menu.php">Космическите хроники</a>
+<a href="menu.php">Космическите хроники</a>
+<a href="menu.php">Космическите хроники</a>
+<a href="menu.php">Космическите хроники</a>
 <a href="chronic_info.php">Информация за планетите и космически тела</a><br/>
-<a href="">Малко интересни факти</a><br/>
+<a href="index.php">Малко интересни факти</a><br/>
 <a href="">Шегички</a><br/>
 <a href="">Космическа литература</a><br/>
 <a href="">Интересни опити</a><br/>
