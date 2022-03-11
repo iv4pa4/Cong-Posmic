824ae012fdd75e2d8cdef8bfd71f3d9fc2a2a386
iv4pa4 <ivayla.panayotova@gmail.com>
2022-03-11 14:22:37 +0200

    hronika menu

---
+++ menu.php
@@ -0,0 +1,17 @@
+<?php
+include("base_connection.php");
+?>
+
+<html>
+<head>
+</head>
+<body>
+<h1>Какво си търсиш?</h1>
+<a href="">Информация за планетите</a><br/>
+<a href="">Малко интересни факти</a><br/>
+<a href="">Шегички</a><br/>
+<a href="">Космическа литература</a><br/>
+<a href="">Интересни опити</a><br/>
+</body>
+
+</html>
