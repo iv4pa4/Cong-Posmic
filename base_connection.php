83b805f2547c4b7753db8bba0f0dca20765abc2c
iv4pa4 <ivayla.panayotova@gmail.com>
2022-03-11 12:36:18 +0200

    connection func

--- base_connection.php
+++ base_connection.php
@@ -12,6 +12,24 @@ if(mysqli_connect_errno){
 	printf("Connection failed: %s/n", mysqli_connect_error());
 	exit();
 }
-
-
+mysqli_query($dblink, "SET NAMES utf8")or die('Няма връзка със сървъра');
+function Select (&query){
+	global $dblink;
+	if(empty($query)){
+		return false;
+	}
+	if(!$res=mysqli_query($dblinkk, $query)){
+		$error='Date:'.date('Y-m-d H:i:s').''.mysqli_error($dblink);
+		die($error);
+	}
+	$data_res=array();
+	if(mysqli_num_rows($res)){
+		while($arr=mysqli_fetch_array($res)){
+			$data_res[]=$arr;
+		}
+	}
+	if(!empty($data_res)){
+		return $data_res;
+	}
+}
 ?>
