40f1a2fe65ef78238d83e5ee7ab379c4b3e6c413
iv4pa4 <ivayla.panayotova@gmail.com>
2022-03-11 12:56:31 +0200

    final fix

--- base_connection.php
+++ base_connection.php
@@ -3,33 +3,35 @@ $hostname='localhost';
 $username='root';
 $password='';
 $db='cong posmic';
-$mysqli=mysqli_init();
-if(!mysqli){
-	die('mysqli_init failed');
+$mysqli = mysqli_init();
+if (!$mysqli) {
+    die('mysqli_init failed');
 }
-$dblink=mysqli_connect($hostname, $username, $password, $db);
-if(mysqli_connect_errno){
-	printf("Connection failed: %s/n", mysqli_connect_error());
-	exit();
+$dblink = mysqli_connect($hostname,$username,$password,$db); 
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
 }
-mysqli_query($dblink, "SET NAMES utf8")or die('Няма връзка със сървъра');
-function Select (&query){
-	global $dblink;
-	if(empty($query)){
-		return false;
-	}
-	if(!$res=mysqli_query($dblink, $query)){
-		$error='Date:'.date('Y-m-d H:i:s').''.mysqli_error($dblink);
-		die($error);
-	}
-	$data_res=array();
-	if(mysqli_num_rows($res)){
-		while($arr=mysqli_fetch_array($res)){
-			$data_res[]=$arr;
+mysqli_query($dblink, "SET NAMES utf8") or die('Няма връзка със сървъра! Опитайте по-късно');
+
+function Select ($query){
+		global $dblink;
+		if (empty($query)) return false;
+		
+		
+		if (!$res = mysqli_query($dblink, $query)) {
+			$error = 'Date: '.date('Y-m-d H:i:s') . ' ' . mysqli_error($dblink);
+			die($error);
 		}
+		
+		
+		$data_res = array();
+		if (mysqli_num_rows($res)) {
+			while ($arr = mysqli_fetch_array($res)) {
+				$data_res[] = $arr;
+			}
+		}
+		if (!empty($data_res))
+			return $data_res;
 	}
-	if(!empty($data_res)){
-		return $data_res;
-	}
-}
 ?>
