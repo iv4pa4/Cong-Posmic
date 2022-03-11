28cd56a4d8c920c3ab5035620a88adeab902c2f4
iv4pa4 <ivayla.panayotova@gmail.com>
2022-03-11 16:34:05 +0200

    planet_template_

--- planet_template.php
+++ planet_template.php
@@ -3,14 +3,22 @@ include("base_connection.php");
 $planet_id=(int) $_GET['pid'];
 $query="SELECT * FROM planets WHERE planet_id=".$planet_id;
 $planet= Select($query);
-if(!$planet){
-	die("Not found")
-}
+if(!$planet) die("Not found");
 ?>
 <html>
 <head>
 </head>
 <body>
-<h1></h1>
+<h1><?=$planet[0]['planet_url'];?></h1>
+<?php
+
+if($planet[0]['planet_photo']){
+	?>
+	<img src="photos/<?=$planet[0]['planet_photo']?>" width="500"/>
+	<?php
+}
+?>
+<h4>Обща информация: <?=$planet[0]['planet_info']; ?></h4>
+
 </body>
 </html>
