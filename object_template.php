fb21c9fbb9f506d3ec804f5183a3b575aa5ba94b
iv4pa4 <ivayla.panayotova@gmail.com>
2022-03-11 21:36:52 +0200

    object_template

---
+++ object_template.php
@@ -0,0 +1,27 @@
+<?php
+include("base_connection.php");
+$object_id=(int) $_GET['pid'];
+$query="SELECT * FROM cosmic_objects WHERE object_id=".$object_id;
+$object= Select($query);
+if(!$object) die("Not found");
+?>
+<html>
+<head>
+<link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet'>
+<link rel="stylesheet" href="style.css?id=2">
+</head>
+<body>
+<h1 class="heading"><?=$object[0]['object_name'];?></h1>
+<div>
+<?php
+
+if($object[0]['object_photo']){
+	?>
+	<img src="photos/<?=$object[0]['object_photo']?>" width="500" align="left" class="pad"/>
+	<?php
+}
+?>
+<h4 class="text_info" align="right">Обща информация: <?=$object[0]['object_info']; ?></h4>
+</div>
+</body>
+</html>
