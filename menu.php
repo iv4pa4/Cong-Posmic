b2dbcfd338971a4349d94a623538af87bacebfc0
iv4pa4 <ivayla.panayotova@gmail.com>
2022-03-11 18:00:08 +0200

    add css
    class buttons

--- menu.php
+++ menu.php
@@ -6,11 +6,11 @@
 </head>
 <body>
 <h1>Какво си търсиш?</h1>
-<a href="chronic_info.php">Информация за планетите и космически тела</a><br/>
-<a href="">Малко интересни факти</a><br/>
-<a href="">Шегички</a><br/>
-<a href="">Космическа литература</a><br/>
-<a href="">Интересни опити</a><br/>
+<a href="chronic_info.php" class="button1">Информация за планетите и космически тела</a><br/>
+<a href="" class="button1">Малко интересни факти</a><br/>
+<a href="" class="button1">Шегички</a><br/>
+<a href="" class="button1">Космическа литература</a><br/>
+<a href="" class="button1">Интересни опити</a><br/>
 </body>
 
 </html>
