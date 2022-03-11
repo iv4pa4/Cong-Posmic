e4d89a1fd7eba4adbdac95aab6dd019b3e1f33e5
iv4pa4 <ivayla.panayotova@gmail.com>
2022-03-11 12:05:43 +0200

    connection error

--- base_connection.php
+++ base_connection.php
@@ -3,5 +3,15 @@ $hostname='localhost';
 $username='root';
 $password='';
 $db='cong posmic';
+$mysqli=mysqli_init();
+if(!mysqli){
+	die('mysqli_init failed');
+}
+$dblink=mysqli_connect($hostname, $username, $password, $db);
+if(mysqli_connect_errno){
+	printf("Connection failed: %s/n", mysqli_connect_error());
+	exit();
+}
+
 
 ?>
