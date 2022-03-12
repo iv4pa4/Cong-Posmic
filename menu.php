6cf46729110a12466fe3497fc64d5648cd54e16d
iv4pa4 <ivayla.panayotova@gmail.com>
2022-03-12 12:00:39 +0200

    fixed

--- menu.php
+++ menu.php
@@ -2,23 +2,26 @@
 <html>
 <head>
 <link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet'>
-<link rel="stylesheet" href="style.css?id=2">
+<link rel="stylesheet" href="style_t.css?id=2">
 </head>
 <body class="background1">
+<div class="zad">
+<a href="index.php" class="button3">Назад</a>
+</div>
 <h1 class="heading">Хроника съдържание:</h1>
 <div align="center">
 <a href="chronic_info.php" class="button1">1-ва глава: Информация за планетите и космически тела</a><br/>\
 <br/>
 <a href="facts.php" class="button1">2-ра глава: Малко интересни факти</a><br/>
 <br/>
-<a href="" class="button1">3-та глава: Space jokes</a><br/>
+<a href="spacejokes.php" class="button1">3-та глава: Space jokes</a><br/>
 <br/>
 <a href="" class="button1">4-та глава: Космическа литература</a><br/>
 <br/>
 <a href="" class="button1">5-та глава: Интересни опити</a><br/>
 <br/>
 </div>
-<a href="index.php" class="button3">Назад</a>
+
 </body>
 
 </html>
---
+++ spacejokes.php
@@ -0,0 +1,32 @@
+<?php
+include("base_connection.php");
+$query = "SELECT * FROM jokes ORDER BY joke_id ASC";
+$jokes = Select ($query);
+?>
+
+<html>
+<head>
+<link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet'>
+<link rel="stylesheet" href="style_t.css?id=2">
+</head>
+<body class="background4">
+<div class="zad">
+<a href="menu.php" class="button3">Назад</a>
+</div>
+<h1 class="heading">Space Jokes:</h1>
+<div align="center">
+<?php
+if(is_array($jokes)){
+	foreach($jokes as $k => $value){
+		?>
+		<h4 class="heading"><?=$value["joke_self"];?></h4><br />
+		<br/>
+		<?php
+	}
+}
+?>
+</div>
+
+</body>
+
+</html>
