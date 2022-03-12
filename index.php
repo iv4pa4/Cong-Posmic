919721cad9bc96bffc19c69cf93973590a844fd6
iv4pa4 <ivayla.panayotova@gmail.com>
2022-03-12 14:25:46 +0200

    nova igra

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <html>
 <head>
 <link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet'>
-<link rel="stylesheet" href="style.css?id=2">
+<link rel="stylesheet" href="style_t.css?id=2">
 </head>
 <body class="background">
 <div class="buttons_div">
 <a href="menu.php" class="button1">Космическите хроники</a>
 <a href="start.html" class="button1">Предизвикай Cong Posmic</a>
+<a href="" class="button1">Стани част от екипажа</a>
 </div>
 </body>
 </html>
